2abd3c33aff9ebf6c6b944a781ae7cbd56e048ef
Iansamaaa <ian.hahaha11@gmail.com>
2017-08-22 14:35:05 +0800

    remake

---
+++ src/pages/INDEX/js/login/triggers.login.js
@@ -0,0 +1,65 @@
+$(function(){
+
+	$(document).ready(function(){
+	
+		$("#subINDEX").bind({
+		
+			click:function(){
+			 validate_login();
+			}
+		
+		});
+		
+		//Keypress Enter
+		$("#username, #password").bind({
+			keypress: function(e){
+				if (e.which == 13){
+					validate_login()
+				}
+			}
+		});
+		
+	}); // End of document ready
+
+}); // End of jQuery function
+
+/**********
+FUNCTIONS
+**********/
+
+//check_fields
+//validate to server
+function validate_login(){
+	
+	//Set Ajax Status
+	var datastring;
+	datastring= {user: $("#username").val(), 
+				pass: $("#password").val(), 
+				e: Number(new Date())};
+	
+	$.ajax({
+		type: "POST",
+		url: "../../../Vb/session/login.asp",
+		data: datastring,
+		async: false,
+		success: function(data){redirect_login(data);
+			}, 
+		error:  function(){
+			toastr.warning("error");}
+	})
+						
+}
+
+//redirect login
+function redirect_login(loginstat){
+	
+	switch (loginstat){
+		
+		case 'x': toastr.warning("error");
+				break;
+		case 'y': window.location="../homepage.asp";
+				break;
+		default:  alert("HAYS");
+		
+	}	
+}
