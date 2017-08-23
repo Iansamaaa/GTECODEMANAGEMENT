2ac2e871802ee3def5648e80ba475d718e923743
Iansamaaa <ian.hahaha11@gmail.com>
2017-08-23 10:09:40 +0800

    Put original folder in the respo for modular purposes

---
+++ Lol/src/Js/CodesJs/codeaddjs.js
@@ -0,0 +1,71 @@
+// JavaScript Document
+$(function(){
+
+	$(document).ready(function(){
+		$("#btnAdd").bind({
+			click:function(){
+			 if($('#ctype').val()=='' || $('#fname').val()=='' || $('#codedesc').val()=='' || $('#version').val()==''|| $('#added').val()=='')
+			{
+				toastr.warning("Fill out Required Fields", "Check Fields"); 
+				return false;
+			}
+			else if(!($('#version').val()).match(/^[0-9.]+$/))
+			{
+			toastr.warning("Type in version Number", "Check Fields"); 
+			return false;
+			}
+			else
+			{
+				code_adding();
+			}
+			}
+		
+		});
+		//Keypress Enter
+		
+	}); // End of document ready
+
+}); // End of jQuery function
+
+/**********
+FUNCTIONS
+**********/
+//check_fields
+//validate to server
+function code_adding(){
+	//Set Ajax Status
+	var datastring;
+	alert($("#codedesc").val());
+	datastring= {ctypes: $("#ctype").val(), 
+				fnames: $("#fname").val(),
+				descs: $("#codedesc").val(),
+				versions: $('#version').val(),
+				addedby:  $('#added').val(),
+				};
+		$.ajax({
+		type: "POST",
+		url: "../Vb/commands/codes/code_func.asp",
+		data: datastring,
+		async: false,
+		success: function(data){code_status(data)
+			$('#addModal1').modal('toggle');
+			 window.setTimeout(function(){location.reload()},2000);
+
+		}, 
+		error:  function(){
+			toastr.warning("Submission Failed");}
+	})					
+}
+//redirect login
+function code_status(codestat){
+	
+	switch (codestat){
+		
+		case 'x': toastr.success("Add Success");
+
+				break;
+		case 'y': toastr.warning("Function Name Already Exists")
+				break;
+		default: toastr.warning("Code not Added");
+	}	
+}
